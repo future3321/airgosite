d7c76b3672c3bdd9f761ba7d159d3a2d96fcb902
future3321 <weilaiwl@hotmail.com>
2024-11-13 14:26:20 +0800

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,7 +11,7 @@ const Footer = () => {
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
           <div className="col-span-1 md:col-span-2">
             <div className="flex items-center">
-              <img src="https://raw.githubusercontent.com/future3321/WEBIMAGES/refs/heads/main/logo.png" alt="Air Basketball" className="h-10 w-10" />
+              <img src="https://gitee.com/airgotech/image_storage/raw/master/logo1.png" alt="Air Basketball" className="h-8 w-10" />
               <span className="ml-2 text-xl font-bold text-gray-900">
                 {t('hero.title')}
               </span>
